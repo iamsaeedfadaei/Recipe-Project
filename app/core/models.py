72ec7ce2d598d849a001221fddb9e31009e8cb80
iamsaeedfadaei <saeed.fadaei1374@gmail.com>
2020-11-03 16:30:21 +0330

    Adding core app, models and test models

---
+++ app/core/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser,\
+                                        PermissionsMixin
+
+
+
+class UserManager(BaseUserManager):
+    #password is none because we can have some user without password
+
+    def create_user(self, email, password=None, **extra_fields):
+        """creating and saving new user"""
+        if not email:
+            raise ValueError('Users must have an Email Address!')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password) # we use this function because passowrd is encrypted and cant be like email.
+        user.save(using=self._db) #we use using for maybe we use multiple db.
+        
+        return user
+
+
+    def create_superuser(self, email, password):
+        """creating and saving new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    """custom user model that supports using email insteadof username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
