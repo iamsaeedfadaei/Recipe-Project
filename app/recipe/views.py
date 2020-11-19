4d4943bd0686a98adf233fd30d6ae150940183e5
iamsaeedfadaei <saeed.fadaei1374@gmail.com>
2020-11-19 20:56:55 +0330

    refactoring recipe views.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,41 @@ from core.models import Tag, Ingredient
 from .serializers import TagSerializer, IngredientSerializer
 
 
+# way 1:
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for user owned recipe attributes."""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        """return objects for the current authenticated user only."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """create a new object for recipe attributes."""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """manage tags in db"""
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """manage migrations in the database."""
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+
+
+
+
+#way 2: has duplicated codes 
+# and because we did write tests for our code we ain't afraid for refactor it and changing it.
 class TagViewSet(viewsets.GenericViewSet,
                  mixins.ListModelMixin,
                  mixins.CreateModelMixin):
