071085d830c064cd62cec222661d50bc3ec6f962
iamsaeedfadaei <saeed.fadaei1374@gmail.com>
2020-11-22 21:32:48 +0330

    recipe updating feature added.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -12,7 +12,7 @@ from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 RECIPE_URL = reverse('recipe:recipe-list')
 
-def deatil_url(recipe_id):
+def detail_url(recipe_id):
     """return recipe detail url"""
     return reverse('recipe:recipe-detail', args=[recipe_id])
 
@@ -99,7 +99,7 @@ class PrivateRecipeApiTests(TestCase):
         recipe.tags.add(sample_tag(user=self.user))  #this is how we add to manytomany field
         recipe.ingredients.add(sample_ingredient(user=self.user))
 
-        url = deatil_url(recipe.id)
+        url = detail_url(recipe.id)
         res= self.client.get(url)
 
         serializer = RecipeDetailSerializer(recipe)  #this is a single object not many=True :)
@@ -160,3 +160,43 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(ingredients.count(), 2)
         self.assertIn(ingredient1, ingredients)
         self.assertIn(ingredient2, ingredients)
+
+    
+    def test_partial_update_recipe(self):
+        """test updating a recipe with patch."""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        new_tag = sample_tag(user=self.user, name='Curry')
+
+        payload = {
+            'title': 'Chicken tikka',
+            'tags': [new_tag.id]
+        }
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 1)
+        self.assertIn(new_tag, tags)
+
+
+    def test_full_update_recipe(self):
+        """test updating recipe with put"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        payload = {
+            'title': 'Spaghetti carbonara',
+            'time_minutes': 25,
+            'price': 5.00
+        }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
